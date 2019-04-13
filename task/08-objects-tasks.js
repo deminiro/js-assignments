02d3fa1793e9ff6e6361b22962574d382f6d283a
deminiro <43145879+deminiro@users.noreply.github.com>
2019-04-13 23:29:40 +0300

    Update 08-objects-tasks.js

--- task/08-objects-tasks.js
+++ task/08-objects-tasks.js
@@ -23,7 +23,17 @@
  *    console.log(r.getArea());   // => 200
  */
 function Rectangle(width, height) {
-    throw new Error('Not implemented');
+//    class Us { 
+//     constructor(width,heigth){
+//         this.width = width;
+//         this.height = height;
+//     }
+    
+//     getArea  () {
+//         return width * height;
+//     }
+//    }
+throw new Error('Not implemented');
 }
 
 
@@ -38,7 +48,7 @@ function Rectangle(width, height) {
  *    { width: 10, height : 20 } => '{"height":10,"width":20}'
  */
 function getJSON(obj) {
-    throw new Error('Not implemented');
+    return JSON.stringify(obj);
 }
 
 
@@ -55,6 +65,7 @@ function getJSON(obj) {
  */
 function fromJSON(proto, json) {
     throw new Error('Not implemented');
+    
 }
 
 
